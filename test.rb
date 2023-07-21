6ac548d68fca297757d7163e66bd7aca1bc48b88
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-07-21 15:28:30 +0400

    lets loop

--- test.rb
+++ test.rb
@@ -154,13 +154,30 @@
 
 # NO 22 Building a Guessing Game
 
-secret_word = "back-end";
-guess = "";
+# secret_word = "back-end";
+# guess = "";
+# guess_count = 0;
+# guess_limit = 3;
+# out_of_guesses = false;
 
-while guess != secret_word
-  puts "Enter guess: "
-  guess = gets.chomp()
 
-end
+# while guess != secret_word and !out_of_guesses
+#   if guess_count < guess_limit
+#     puts "Enter guess: "
+#     guess = gets.chomp()
+#     guess_count += 1;
+#   else
+#     out_of_guesses = true;
+#   end
+# end
 
-puts "you won"
+# if out_of_guesses
+#   puts "You lose"
+# else
+#   puts "You won"
+# end
+
+
+# NO 23 For Loops
+
+
