9a8caa77597fa53680ab358beb952b42b917aefb
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-07-22 20:26:27 +0400

    finish ruby

--- test.rb
+++ test.rb
@@ -316,35 +316,75 @@ is here
 
 # NO 32 Builiding a Quiz
 
-class Question
-  attr_accessor :prompt, :answer
-  def initialize(prompt, answer)
-    @prompt = prompt
-    @answer = answer
-  end
-end
+# class Question
+#   attr_accessor :prompt, :answer
+#   def initialize(prompt, answer)
+#     @prompt = prompt
+#     @answer = answer
+#   end
+# end
 
-p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
-p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
-p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
+# p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
+# p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
+# p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
 
-questions = [
-  Question.new(p1, "a"),
-  Question.new(p2, "c"),
-  Question.new(p3, "b")
-]
+# questions = [
+#   Question.new(p1, "a"),
+#   Question.new(p2, "c"),
+#   Question.new(p3, "b")
+# ]
 
-def run_test(questions)
-  answer = "";
-  score = 0;
-  for question in questions
-    puts question.prompt
-    answer = gets.chomp()
-    if answer == question.answer
-      score += 1
-    end
-  end
-  puts ("You got " + score.to_s + "/" + questions.length().to_s)
-end
+# def run_test(questions)
+#   answer = "";
+#   score = 0;
+#   for question in questions
+#     puts question.prompt
+#     answer = gets.chomp()
+#     if answer == question.answer
+#       score += 1
+#     end
+#   end
+#   puts ("You got " + score.to_s + "/" + questions.length().to_s)
+# end
 
-run_test(questions)
+# run_test(questions)
+
+# NO 33 Inheritance
+
+# class Chef
+#   def make_chicken
+#     puts "The chef makes chicken"
+#   end
+#   def make_salad
+#     puts "The chef makes salad"
+#   end
+#   def make_special_dish
+#     puts "The chef makes bbq ribs"
+#   end
+# end
+
+# class ItalianChef < Chef
+#   def make_special_dish
+#     puts "The Italian Chef make a big burger"
+#   end
+#   def make_pasta
+#     puts "The Italian Chef make a pasta"
+#   end
+# end
+
+# chef = Chef.new()
+# chef.make_chicken
+
+# italian_chef = ItalianChef.new()
+# italian_chef.make_special_dish;
+# italian_chef.make_pasta;
+
+# NO 34 Modules
+
+# require_relative "useful_tools.rb"
+# include Tools
+
+# Tools.sayhi("Giorgi")
+
+# NO 35 Interactive Ruby (irb)
+
