3681496b24da4b10a47e97fff197bfb32b2c1227
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-09-16 21:43:34 +0400

    LR3

--- test.rb
+++ test.rb
@@ -813,3 +813,39 @@ puts "Comments are fun"
 
 # Inheritance
 
+# class Chef
+#   def make_chicken
+#     puts "The chef makes chicken"
+#   end
+#   def make_salad
+#     puts "The chef makes salad"
+#   end
+#   def make_special_dish
+#     puts "The chef makes bbq ribs"
+#   end
+# end
+
+# class ItalianChef < Chef
+#   def make_special_dish 
+#     puts "Italian chef make special dish"
+#   end
+#   def make_pasta
+#     puts "The chef make pasta"
+#   end
+# end
+
+# chef = Chef.new()
+# chef.make_chicken
+
+# italian_chef = ItalianChef.new()
+# italian_chef.make_chicken
+# italian_chef.make_special_dish
+
+
+# require_relative "useful_tools.rb"
+# include Tools
+
+# Tools.sayhi("Giorgi Mindiashvili")
+
+# Interactive Ruby(irb)
+
--- useful_tools.rb
+++ useful_tools.rb
@@ -9,3 +9,16 @@
 
 # include Tools
 # Tools.sayhi("Giviko")
+
+
+# module Tools
+#   def sayhi(name)
+#     puts "Hi #{name}"
+#   end
+#   def saybye(name)
+#     puts "Bye #{name}"
+#   end
+# end
+
+# include Tools
+# Tools.sayhi("Giorgi")
